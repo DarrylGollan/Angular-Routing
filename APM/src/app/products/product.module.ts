d5ae012371d3dbf5183f8e42bd95b11b38ed6459
DarrylGollan <darryl.gollan@gmail.com>
2021-01-20 15:02:55 -0500

    Completed module 4 on Pluralsight: Angular Routing.

---
+++ APM/src/app/products/product.module.ts
@@ -0,0 +1,23 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent }
+    ])
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductEditComponent
+  ]
+})
+export class ProductModule { }
